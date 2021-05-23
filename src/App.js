25712827e5b78fe9710025131e5503f88cf5a3a4
George Gebbett <george.gebbett1@gmail.com>
2021-05-23 12:46:16 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import marked from 'marked'
 
 function App() {
   return (
@@ -22,4 +24,92 @@ function App() {
   );
 }
 
-export default App;
+
+class MarkdownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorText: '',
+      markedUp: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return (
+        <div className='App'>
+            <div className='App-header'>
+                <MarkdownHeader/>
+            </div>
+            <div className='flex-container'>
+                <div className='Editor'>
+                    <Editor editorText={this.state.editorText} handleChange={this.handleChange}/>
+                </div>
+                <div className='Renderer'>
+                    <Renderer markedUpText={this.state.markedUp}/>
+                </div>
+            </div>
+        </div>
+  )
+  }
+
+  handleChange(event){
+    this.setState({
+        editorText: event.target.value,
+        markedUp: marked(event.target.value)
+    });
+    console.log(this.state.markedUp);
+  }
+}
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <div className='editorArea'>
+        <h2>Editor</h2>
+        <textarea className='editBox' id='editor' onChange={this.props.handleChange} value={this.props.editorText}/>
+      </div>
+        )
+  }
+}
+
+class Renderer extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div className='renderArea'>
+                <h2>Result</h2>
+                <div
+                    id='preview'
+                    dangerouslySetInnerHTML={{
+                        __html: this.props.markedUpText
+                    }}
+                />
+            </div>
+        )
+    }
+}
+
+class MarkdownHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render () {
+    return (
+          <h1>Markdown Translator!</h1>
+    )
+  }
+}
+
+export default MarkdownPreviewer;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MarkdownPreviewer from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MarkdownPreviewer />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
